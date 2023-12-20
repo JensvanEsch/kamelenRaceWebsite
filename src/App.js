e1f5ddbb9336e5406b409edc7bfde6774e731ae9
Jens van Esch <j.d.n.v.esch@student.tue.com>
2023-12-20 21:22:21 +0100

    Website begin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import "./style.css";
 
 function App() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "Build/build.loader.js",
+    dataUrl: "Build/build.data.unityweb",
+    frameworkUrl: "Build/build.framework.js.unityweb",
+    codeUrl: "Build/build.wasm.unityweb",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="w-[351px] h-[624px] mx-auto mt-[40px]">
+        <Unity unityProvider={unityProvider} className="w-full h-full" />
+      </div>
+      {/* Advertentieblock onder */}
+      <div className="relative w-[351px] h-10 mx-auto bg-black b-0">
+        <p className="text-white">advertentiehier</p>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ], 
+  // height: [
+  //   '278px' : '278px',
+  // ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
