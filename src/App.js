9d3b29a531433734098bf0e4c409fc9420f3d2fa
Jens van Esch <j.d.n.v.esch@student.tue.com>
2023-12-21 20:02:02 +0100

    Voor telefoon g

--- src/App.js
+++ src/App.js
@@ -11,13 +11,16 @@ function App() {
   });
 
   return (
-    <div className="bg-slate-600 h-screen w-screen pt-8">
-      <div className="w-[351px] h-[624px] mx-auto rounded-lg">
-        <Unity unityProvider={unityProvider} className="w-full h-full rounded-t-lg" />
+    <div className="bg-slate-600 h-screen w-screen sm:h-screen sm:w-screen sm:pt-8">
+      <div className="pb-12 w-screen h-screen sm:w-[351px] sm:h-[624px] sm:mx-auto rounded-lg">
+        <Unity
+          unityProvider={unityProvider}
+          className="w-full h-full rounded-t-lg"
+        />
       </div>
-      
+
       {/* Advertentieblock onder */}
-      <div className="flex w-[351px] h-12 rounded-b-lg mx-auto bg-black b-0">
+      <div className="flex -mt-12 h-12 min-w-[351px] sm:w-[351px] mx-auto bg-black b-0">
         <h1 className="text-white mx-auto my-auto">advertentiehier</h1>
       </div>
     </div>
